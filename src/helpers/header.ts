9c805f4f442dda83380bfd98032d1e89f1c0a00b
qinhao <82450473@qq.com>
2025-11-25 15:34:04 +0800

    处理headers

---
+++ src/helpers/header.ts
@@ -0,0 +1,23 @@
+import { isPlainObject } from './utils'
+// 由于header可能设置小写的，所以需要做处理
+function normalizeHeaderName(headers: any, normalizedName: string) {
+  if (!headers) return
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizedName && name.toUpperCase() === normalizedName.toUpperCase()) {
+      headers[normalizedName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+
+export function processHeaders(headers: any, data: any): any {
+  normalizeHeaderName(headers, 'Content-Type')
+
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-Type']) {
+      headers['Content-Type'] = 'application/json;charset=utf-8'
+    }
+  }
+
+  return headers
+}
