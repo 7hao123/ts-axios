5e9dba57da62f72a85e405b7c7a565a9868bd596
qinhao <82450473@qq.com>
2025-11-25 15:14:17 +0800

    处理url和data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
+import { transformRequest } from './helpers/data'
+import { buildURL } from './helpers/url'
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 function axios(config: AxiosRequestConfig) {
+  processConfig(config)
   xhr(config)
 }
+function processConfig(config: AxiosRequestConfig) {
+  // 对config里面的url进行处理
+  config.url = transformURL(config)
+  config.data = transformData(config)
+}
+
+function transformURL(config: AxiosRequestConfig) {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+function transformData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
 export default axios
